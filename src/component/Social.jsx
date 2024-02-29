040e2c9593509cc65a4a179704890e77d061944e
maulmshr <maulmshr@gmail.com>
2024-03-01 00:34:14 +0700

    s

--- src/component/Social.jsx
+++ src/component/Social.jsx
@@ -5,7 +5,7 @@ function Social() {
         <div className="social pt-3">
             <ul>
                 <li >
-                    <a href="http://" className="rounded-circle p-2 me-2 bg-dark"><img  src="../public/images/linkedin.png" alt="" /></a>
+                    <a href="http://" className="rounded-circle p-2 me-2 bg-dark"><img  src="/images/linkedin.png" alt="" /></a>
                     <a href="http://" className="rounded-circle p-2 me-2 bg-dark"><img  src="../public/images/github.png" alt="" /></a>
                     <a href="http://" className="rounded-circle p-2 me-2 bg-dark"><img  src="../public/images/whatsapp.png" alt="" /></a>
                     <a href="http://" className="rounded-circle p-2 me-2 bg-dark"><img  src="../public/images/instagram.png" alt="" /></a>
